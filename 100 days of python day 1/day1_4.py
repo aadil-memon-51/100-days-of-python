8245b8d75cfd1b1ab05b194621aebffcc38722e2
AADiL SAleem <aadil-memon-51>
2021-09-03 21:25:39 +0500

    last day 1 commit

---
+++ 100 days of python day 1/day1_4.py
@@ -0,0 +1,6 @@
+a=input("a: ")
+b=input("b: ")
+# swapping values
+a,b=b,a
+print("a: "+a)
+print("b: "+b)
---
+++ 100 days of python day 1/day1_project.py
@@ -0,0 +1,4 @@
+print("Welcome to Band Name Generator: ")
+city=input("Enter the city you grew up in: \n")
+p_name=input("Enter the name of your pet: \n")
+print("Your band name is : "+city+" "+p_name)
