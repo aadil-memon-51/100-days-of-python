8adebb08ad387e9e27d5142b99192f6c74e3de76
AADiL SAleem <aadil-memon-51>
2021-09-07 11:19:57 +0500

    day 7

--- 100 days of python day7 HangMan Project/HangmanProject.py
+++ 100 days of python day7 HangMan Project/HangmanProject.py
@@ -1,12 +1,310 @@
-#day7_1
+from os import system
 import random
-word_list=["aadil","waiz","hammad","ali"]
-word_chosen=random.choice(word_list)
-guess=input("Guess a Letter ").lower()
-for i in range(len(word_chosen)):
-    if guess==word_chosen[i]:
-        print("You guessed it right")
-    else:
-        print("Wrong guess!")
 
-#day7_2
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+logo = ''' 
+ _                                             
+| |                                            
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                    __/ |                      
+                   |___/    '''
+print(logo)
+word_list = [
+'abruptly',
+'absurd',
+'abyss',
+'affix',
+'askew',
+'avenue',
+'awkward',
+'axiom',
+'azure',
+'bagpipes',
+'bandwagon',
+'banjo',
+'bayou',
+'beekeeper',
+'bikini',
+'blitz',
+'blizzard',
+'boggle',
+'bookworm',
+'boxcar',
+'boxful',
+'buckaroo',
+'buffalo',
+'buffoon',
+'buxom',
+'buzzard',
+'buzzing',
+'buzzwords',
+'caliph',
+'cobweb',
+'cockiness',
+'croquet',
+'crypt',
+'curacao',
+'cycle',
+'daiquiri',
+'dirndl',
+'disavow',
+'dizzying',
+'duplex',
+'dwarves',
+'embezzle',
+'equip',
+'espionage',
+'euouae',
+'exodus',
+'faking',
+'fishhook',
+'fixable',
+'fjord',
+'flapjack',
+'flopping',
+'fluffiness',
+'flyby',
+'foxglove',
+'frazzled',
+'frizzled',
+'fuchsia',
+'funny',
+'gabby',
+'galaxy',
+'galvanize',
+'gazebo',
+'giaour',
+'gizmo',
+'glowworm',
+'glyph',
+'gnarly',
+'gnostic',
+'gossip',
+'grogginess',
+'haiku',
+'haphazard',
+'hyphen',
+'iatrogenic',
+'icebox',
+'injury',
+'ivory',
+'ivy',
+'jackpot',
+'jaundice',
+'jawbreaker',
+'jaywalk',
+'jazziest',
+'jazzy',
+'jelly',
+'jigsaw',
+'jinx',
+'jiujitsu',
+'jockey',
+'jogging',
+'joking',
+'jovial',
+'joyful',
+'juicy',
+'jukebox',
+'jumbo',
+'kayak',
+'kazoo',
+'keyhole',
+'khaki',
+'kilobyte',
+'kiosk',
+'kitsch',
+'kiwifruit',
+'klutz',
+'knapsack',
+'larynx',
+'lengths',
+'lucky',
+'luxury',
+'lymph',
+'marquis',
+'matrix',
+'megahertz',
+'microwave',
+'mnemonic',
+'mystify',
+'naphtha',
+'nightclub',
+'nowadays',
+'numbskull',
+'nymph',
+'onyx',
+'ovary',
+'oxidize',
+'oxygen',
+'pajama',
+'peekaboo',
+'phlegm',
+'pixel',
+'pizazz',
+'pneumonia',
+'polka',
+'pshaw',
+'psyche',
+'puppy',
+'puzzling',
+'quartz',
+'queue',
+'quips',
+'quixotic',
+'quiz',
+'quizzes',
+'quorum',
+'razzmatazz',
+'rhubarb',
+'rhythm',
+'rickshaw',
+'schnapps',
+'scratch',
+'shiv',
+'snazzy',
+'sphinx',
+'spritz',
+'squawk',
+'staff',
+'strength',
+'strengths',
+'stretch',
+'stronghold',
+'stymied',
+'subway',
+'swivel',
+'syndrome',
+'thriftless',
+'thumbscrew',
+'topaz',
+'transcript',
+'transgress',
+'transplant',
+'triphthong',
+'twelfth',
+'twelfths',
+'unknown',
+'unworthy',
+'unzip',
+'uptown',
+'vaporize',
+'vixen',
+'vodka',
+'voodoo',
+'vortex',
+'voyeurism',
+'walkway',
+'waltz',
+'wave',
+'wavy',
+'waxy',
+'wellspring',
+'wheezy',
+'whiskey',
+'whizzing',
+'whomever',
+'wimpy',
+'witchcraft',
+'wizard',
+'woozy',
+'wristwatch',
+'wyvern',
+'xylophone',
+'yachtsman',
+'yippee',
+'yoked',
+'youthful',
+'yummy',
+'zephyr',
+'zigzag',
+'zigzagging',
+'zilch',
+'zipper',
+'zodiac',
+'zombie',
+]
+lives=len(stages)-1
+word_chosen=random.choice(word_list)
+display=[]
+for letter in word_chosen:
+    display+="_"
+print(display)
+while not display==word_chosen:
+    guess=input("\nGuess a Letter ").lower()
+    system('cls')
+    if guess in display:
+        print(f"You\'ve already guessed {guess} letter")
+    for i in range(len(word_chosen)):
+        # print(f"Current position: {i}\n Current letter: {word_chosen[i]}\n Guessed letter: {guess}")
+        if word_chosen[i]==guess:
+            display[i]=word_chosen[i]
+    print(display)
+    if guess not in word_chosen:
+        print(f"You Guessed {guess} that is not in the word, You lose a life.")
+        lives-=1
+    print(stages[lives])
+    if "_" not in display:
+        print("\nYou Win!")
+        break
+    if lives==0:
+        print("\n You Lose!")
+        break
