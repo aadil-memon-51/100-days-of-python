b29f15336d5bcf7409fbb7853191b91123b46fa9
AADiL SAleem <aadil-memon-51>
2021-09-11 18:55:40 +0500

    day 10

---
+++ 100 days of python day 10/day10_Calc.py
@@ -0,0 +1,43 @@
+logo = """
+ _____________________
+|  _________________  |
+| | Pythonista   0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
+|_____________________|
+"""
+def add(n1,n2):
+    return n1+n2
+def subtract(n1,n2):
+    return n1-n2
+def multiply(n1,n2):
+    return n1*n2
+def divide(n1,n2):
+    return n1/n2
+cal={'+':add,'-':subtract,'*':multiply,'/':divide}
+def calculator():
+    print(logo)
+    num1=float(input("What\'s the first number: "))
+    for symbol in cal:
+        print(symbol)
+    type='y'
+    while type=='y':
+        pick_symbol=input("pick an operation: ")
+        num2=float(input("What\'s the next number: "))
+        calfunc=cal[pick_symbol]
+        answer=calfunc(num1,num2)
+        print(f"{num1}{pick_symbol}{num2}={answer}")
+        type=input(f"Type 'y' to continue calculation with {answer}, or type 'n' to exit:  ")
+        if type=='y':
+            num1=answer
+        else:
+            calculator()
+calculator()
